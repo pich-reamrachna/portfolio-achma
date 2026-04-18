3eade2123255cf5f722db0cfd5728178b23d0059
rachna <pichreamr@gmail.com>
2026-04-18 20:17:25 +0700

    fix: change title and fix wrong image ext

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,7 +17,7 @@
 
 	<meta property="og:type" content="website" />
 	<meta property="og:site_name" content="Achma Portfolio" />
-	<meta property="og:title" content="Achma | 3D Frontend Developer" />
+	<meta property="og:title" content="Achma | Portfolio" />
 	<meta
 		property="og:description"
 		content="Interactive 3D portfolio showcasing web and cybersecurity projects."
@@ -26,7 +26,7 @@
 	<meta property="og:image" content="https://achma.site/og-cover.jpg" />
 
 	<meta name="twitter:card" content="summary_large_image" />
-	<meta name="twitter:title" content="Achma | 3D Frontend Developer" />
+	<meta name="twitter:title" content="Achma | Portfolio" />
 	<meta
 		name="twitter:description"
 		content="Interactive 3D portfolio showcasing frontend projects, UI experiments, and creative web experiences."
